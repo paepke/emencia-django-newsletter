1502c0cb595c036b72943c1d07da7ec9ea3dcb30
Marco <marco@cuthemustard.com>
2012-06-08 01:39:53 -0400

    woops, forgot to remove commented code
    Conflicts:
    	emencia/admin/contact.py

--- emencia/admin/contact.py
+++ emencia/admin/contact.py
@@ -137,7 +137,6 @@ class ContactAdmin(admin.ModelAdmin):
 
         context = {'title': _('Contact importation'),
                    'opts': opts,
-                   #~ 'root_path': self.admin_site.root_path,
                    'root_path': reverse('admin:index'),
                    'app_label': opts.app_label}
 
